0a4bdafbc51738498d67c4060cbe61d5d0bdfbc8
ousmaneacs2018 <ousmane.balde@hotmail.fr>
2018-07-27 11:07:46 +0200

    dossier creation theme

---
+++ creationTheme/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head profile="http://gmpg.org/xfn/11">   
+<title>
+
+    <?php bloginfo('name') ?><?php if ( is_404() ) : ?> &raquo; <?php _e('Not Found') ?><?php elseif ( is_home() ) : ?> &raquo; 
+    <?php bloginfo('description') ?><?php else : ?><?php wp_title() ?><?php endif ?>
+
+</title>   
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" /> 
+<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats --> 
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" /> 
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" /> 
+<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" /> 
+<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" /> 
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" /><?php wp_head(); ?>   <?php wp_get_archives('type=monthly&format=link'); ?> <?php //comments_popup_script(); <?php wp_head(); ?>   
+</head> 
+
+<body class="h100vh">
+<header class="dflex bgblock2 h20vh">
+    <div class="w50ps d_flexcenter">
+        <a href="<?php bloginfo('url'); ?>"><img class="h20vh"src="./wp-content\themes\creationTheme\img/logoacs.svg"></a>        
+    </div>
+    <div class="w50ps dflex flex_end"> <img class="h2vh" src="./wp-content\themes\creationTheme\img/iconeli.svg"><?php wp_loginout(); ?> <?php wp_register(); ?></div>
+    
+</header>
+
